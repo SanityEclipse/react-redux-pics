9d56e7e3b2421f06b3e4dc39e6fb9f2684568bfc
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2020-05-08 17:03:48 -0600

    updated project with .env to  conceal API key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID HR1Yow3xv0XDdP8MIEfu_GTdkCDZfLgcwr-unWACxnk'
+        Authorization: process.env.REACT_APP_UNSPLASH_API_KEY
     }
 });
